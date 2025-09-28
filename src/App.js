8d40ee6569eaec4f7493dfd72d17c9323d9f41ac
Edu <903723@unizar.es>
2025-09-28 20:00:56 +0200

    LOGO OK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./logo.png" alt="logo" />
+        <img src={logo} className="App-logoLima" alt="logo" />
         <p>
           CB LIMA LIMÓN
         </p>
