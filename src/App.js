293a831547038f446cf3ec23110b6ac22d80247b
Edu <903723@unizar.es>
2025-09-28 18:36:00 +0200

    logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="logo.png" alt="logo" />
+        <img src="./logo.png" alt="logo" />
         <p>
           CB LIMA LIMÓN
         </p>
